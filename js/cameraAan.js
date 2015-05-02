0f8e1aae8aba919f7378fd782235b7bd75369b3e
Abdisalaam <qayed2004@hotmail.com>
2015-05-02 15:09:06 +0200

    Update cameraAan.js

--- js/cameraAan.js
+++ js/cameraAan.js
@@ -4,16 +4,16 @@ $(document).ready(function(){
 	document.addEventListener('deviceready', onDeviceReady, false);
 	 function onDeviceReady(){
             navigator.camera.getPicture(onSuccess, onFail, {quality:20, destinationType: Camera.DestinationType.FILE_URI
-});
-            function onSuccess(imageData){
+            
+});}
+
+
+   
+    });
+function onSuccess(imageData){
                 var image=document.getElementById('popImage');
                 image.src='data:image/jpeg;base64,'+imageData;
             }
             function onFail(message){
                 alert(message);
             }
-    }
-
-
-   
-    });
